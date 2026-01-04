f7aff9297b957d79a597c266735468fff1656c5b
Hector Brito <hectorbritodev@gmail.com>
2026-01-03 21:24:16 -0300

    validation tests

---
+++ functions/test_get_files_info.py
@@ -0,0 +1,14 @@
+import unittest
+from get_files_info import get_files_info
+
+class TestGetFilesInfo(unittest.TestCase):
+    def test_invalid_directory_root(self):
+        results = get_files_info("calculator", "/bin")
+        self.assertEqual(results, 'Error: Cannot list "/bin" as it is outside the permitted working directory')
+
+    def test_invalid_directory_outside(self):
+        results = get_files_info('calculator', '../')
+        self.assertEqual(results, 'Error: Cannot list "../" as it is outside the permitted working directory')
+
+if __name__ == "__main__":
+    unittest.main()
